a3559eb1d93e483afc8fe1ac81e8a9878339ae8a
ZobairSoodi <boukkour.mohamed.solicode@gmail.com>
2023-06-29 02:17:20 +0000

    Update ShowRole.jsx

--- frontend/src/pages/Game/ShowRole.jsx
+++ frontend/src/pages/Game/ShowRole.jsx
@@ -2,25 +2,39 @@ import { useState } from "react";
 
 function ShowRole({ selectedSubject, players }) {
     const [index, setIndex] = useState(0);
+    const [show, setShow] = useState(false);
     console.log(players);
     return <div>
-        {!players[index].isObtrusive ?
-            <div>
-                <p>
-                    {players[index].name}
-                </p>
-                <h1>
-                    Subject:
-                </h1>
-                <p>
-                    {selectedSubject.name}
-                </p>
-            </div> :
-            <div>
-                ydk fih jhjhhjhjhjhj
-            </div>
+        {show ? <>
+            {!players[index].isObtrusive ?
+                <div>
+                    <p>
+                        {players[index].name}
+                    </p>
+                    <h1>
+                        Subject:
+                    </h1>
+                    <p>
+                        {selectedSubject.name}
+                    </p>
+                </div> :
+                <div>
+                    ydk fih jhjhhjhjhjhj
+                </div>
+            }
+        </> : <div>
+            give the device to {players[index].name}
+        </div>
         }
-        {players.length - 1 !== index ? <button onClick={() => setIndex(index + 1)}>NEXT</button> : ""}
+        {show ? <div>
+            {players.length - 1 !== index ? <button onClick={() => {
+                setIndex(index + 1);
+                setShow(false);
+            }}>NEXT</button> : ""}
+        </div> : <div>
+            <button onClick={() => setShow(true)}>NEXT</button>
+        </div>
+        }
     </div>
 }
 
